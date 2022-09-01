6751e205550fcfabf5d4a653dc0560dd452e4e83
Erika <erikaduarte2014@hotmail.com-v>
2022-09-01 09:12:39 -0500

    Calculadora finalizada

--- script.js
+++ script.js
@@ -1,55 +1,17 @@
-// alert("Este programa es de una calculadora básica");
-// function suma(a,b){
-//   return a+b;
-// }
 
-// function resta(a,b){
-//   return a-b;
-// }
 
-// function multiplicacion(a,b){
-//   return a*b;
-// }
 
-// function division(a,b){
-//   return a/b;
-// }
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
 
-/*let seleccion = parseInt(prompt("Seleccione la operación que desee\n 1: Suma\n 2:Resta\n 3:Multiplicación\n 4:División\n 5:Salir\n"));
+const display = new Display(displayValorAnterior, displayValorActual);
 
-while (seleccion<5){
-let n1 = parseInt(prompt("Ingrese el número 1"));
-let n2 = parseInt(prompt("Ingrese el número 2"));
-var s
-if(seleccion===1){
-  alert("Seleccionaste de operación la suma y el resultado es  "+suma(n1,n2))
-}else if(seleccion===2){
-   alert("Seleccionaste de operación la resta y el resultado es  "+resta(n1,n2))
-}else if(seleccion===3){
-   alert("Seleccionaste de operación la multiplicación y el resultado es  "+multiplicacion(n1,n2))
-}else if(seleccion===4){
-   alert("Seleccionaste de operación la división y el resultado es  "+division(n1,n2))
-}else{
-  alert("Ingresaste un valor inválido")
-}
-seleccion = parseInt(prompt("Seleccione la operación que desee\n 1: Suma\n 2:Resta\n 3:Multiplicación\n 4:División\n 5:Salir\n"));
-}
-alert("Decidiste salir Hasta Pronto")*/
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
 
-class Calculadora {
-  sumar(num1, num2) {
-      return num1 + num2;
-  }
-
-  restar(num1, num2) {
-      return num1 - num2;
-  }
-
-  dividir(num1, num2) {
-      return num1 / num2;
-  }
-
-  multiplicar(num1, num2) {
-      return num1 * num2;
-  }
-} 
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
